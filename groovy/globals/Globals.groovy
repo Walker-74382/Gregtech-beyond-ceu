32a1a85eddddddcfca6ce86debdb6632537cadfd
BLocky104 <farmermick13@web.de>
2025-03-21 17:44:23 +0100

    Create Globals.groovy

---
+++ groovy/globals/Globals.groovy
@@ -0,0 +1,24 @@
+package globals
+
+import com.cleanroommc.groovyscript.api.IIngredient
+import gregtech.api.fluids.store.FluidStorageKeys;
+import gregtech.api.fluids.store.FluidStorage;
+import gregtech.api.unification.material.properties.*
+import gregtech.api.unification.material.Material;
+
+
+import static gtb.api.recipes.GTBRecipeMaps.*;
+import static gtb.api.unification.materials.GTBMaterials.*;
+import static gtb.common.item.GTBMetaItems.*;
+import static gregtech.api.fluids.FluidConstants.*;
+
+
+class Globals {
+
+    public static void main (String[] args) {
+
+    }
+
+
+
+}
