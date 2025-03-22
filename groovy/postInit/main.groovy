a542b4a4abfce3ce2f4ae2a35b02a2676de45474
BLocky104 <farmermick13@web.de>
2025-03-22 14:45:56 +0100

    Wetware circuitry

---
+++ groovy/postInit/main.groovy
@@ -0,0 +1,22 @@
+
+// Nanoprocessor * 2
+mods.gregtech.circuit_assembler.removeByInput(600, [metaitem('circuit_board.advanced'), metaitem('plate.nano_central_processing_unit'), metaitem('component.smd.resistor') * 8, metaitem('component.smd.capacitor') * 8, metaitem('component.smd.transistor') * 8, metaitem('wireFineElectrum') * 8], [fluid('soldering_alloy') * 72 * 72])
+
+// Nanoprocessor * 2
+mods.gregtech.circuit_assembler.removeByInput(600, [metaitem('circuit_board.advanced'), metaitem('plate.nano_central_processing_unit'), metaitem('component.smd.resistor') * 8, metaitem('component.smd.capacitor') * 8, metaitem('component.smd.transistor') * 8, metaitem('wireFineElectrum') * 8], [fluid('tin') * 144 * 144])
+
+// Nanoprocessor * 2
+mods.gregtech.circuit_assembler.removeByInput(600, [metaitem('circuit_board.advanced'), metaitem('plate.nano_central_processing_unit'), metaitem('component.advanced_smd.resistor') * 2, metaitem('component.advanced_smd.capacitor') * 2, metaitem('component.advanced_smd.transistor') * 2, metaitem('wireFineElectrum') * 8], [fluid('tin') * 144 * 144])
+
+// Nanoprocessor * 2
+mods.gregtech.circuit_assembler.removeByInput(600, [metaitem('circuit_board.advanced'), metaitem('plate.nano_central_processing_unit'), metaitem('component.advanced_smd.resistor') * 2, metaitem('component.advanced_smd.capacitor') * 2, metaitem('component.advanced_smd.transistor') * 2, metaitem('wireFineElectrum') * 8], [fluid('soldering_alloy') * 72 * 72])
+
+// Nanoprocessor * 4
+mods.gregtech.circuit_assembler.removeByInput(9600, [metaitem('circuit_board.advanced'), metaitem('plate.advanced_system_on_chip'), metaitem('wireFineElectrum') * 4, metaitem('boltPlatinum') * 4], [fluid('soldering_alloy') * 72 * 72])
+
+// Nanoprocessor * 4
+mods.gregtech.circuit_assembler.removeByInput(9600, [metaitem('circuit_board.advanced'), metaitem('plate.advanced_system_on_chip'), metaitem('wireFineElectrum') * 4, metaitem('boltPlatinum') * 4], [fluid('tin') * 144 * 144])
+
+
+
+        
