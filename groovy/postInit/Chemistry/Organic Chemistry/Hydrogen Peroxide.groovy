00d0e8965b58c33e5c16f2209ac1f9c5f93a03a1
BLocky104 <farmermick13@web.de>
2025-03-22 14:46:42 +0100

    Hydrogen Peroxide Line

---
+++ groovy/postInit/Chemistry/Organic Chemistry/Hydrogen Peroxide.groovy
@@ -0,0 +1,40 @@
+DISTILLATION_RECIPES = recipemap('distillation_tower')
+CHEMICAL_RECIPES = recipemap('chemical_reactor')
+
+        DISTILLATION_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('coal_tar')*1000)
+                .fluidOutputs(fluid('phenol')*100)
+                .fluidOutputs(fluid('naphthalene')*400)
+                .fluidOutputs(fluid('anthracene')*100)
+                .fluidOutputs(fluid('hydrogen_sulfide')*200)
+                .chancedOutput(metaitem('dustCoke')*1, 2000, 20)
+                .duration(100)
+                .EUt(780)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('ethyl_benzene')*1000)
+                .inputs(metaitem('dustPhthalicAnhydride')* 15)
+                .fluidOutputs(fluid('ethylanthraquinone')*1000)
+                .fluidOutputs(fluid('water')*1000)
+                .duration(200)
+                .EUt(800)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('ethylanthraquinone')*1000)
+                .fluidInputs(fluid('hydrogen')*1000)
+                .fluidOutputs(fluid('ethylanthrahydroquinone')*1000)
+                .duration(200)
+                .EUt(100)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('ethylanthrahydroquinone')*1000)
+                .notConsumable(fluid('anthracene')*1000)
+                .fluidInputs(fluid('oxygen')*2000)
+                .fluidOutputs(fluid('hydrogen_peroxide')*2000)
+                .fluidOutputs(fluid('ethylanthraquinone')*1000)
+                .duration(299)
+                .EUt(800)
+                .buildAndRegister();
