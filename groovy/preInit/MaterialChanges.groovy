931d1c6df3a47166ffb8b83ef2596b8eaefe0188
BLocky104 <farmermick13@web.de>
2025-03-23 15:20:00 +0100

    make MaterialChanges HIGHEST Priority

--- groovy/preInit/MaterialChanges.groovy
+++ groovy/preInit/MaterialChanges.groovy
@@ -3,14 +3,14 @@ package preInit;
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import gregtech.api.unification.material.event.MaterialEvent;
 
-import material.GTBMaterials
+import material.GTBMaterial;
 
 log.infoMC("Registering new material event listener")
 
-eventManager.listen(EventPriority.LOWEST) {
+eventManager.listen(EventPriority.HIGHEST) {
     MaterialEvent event ->
 
         log.infoMC("Starting new material registration")
         
-        GTBMaterials.init();
+        GTBMaterial.init();
 }
