9def81d5214eff62ff45a42cfe2d6747c8660c77
BLocky104 <farmermick13@web.de>
2025-03-23 15:19:25 +0100

    fix polyethylene plastic name

--- groovy/postInit/Chemistry/Polymers/Polyethylene.groovy
+++ groovy/postInit/Chemistry/Polymers/Polyethylene.groovy
@@ -5,7 +5,7 @@ import static gtb.api.recipes.GTBRecipeMaps.*;
         POLYMERIZATION_TANK_RECIPES.recipeBuilder()
                 .fluidInputs(fluid('air')*1000)
                 .fluidInputs(fluid('ethylene')*144)
-                .fluidInputs(fluid('polyethylene')*144)
+                .fluidInputs(fluid('plastic')*144)
                 .duration(500)
                 .EUt(800)
                 .buildAndRegister();
@@ -13,31 +13,31 @@ import static gtb.api.recipes.GTBRecipeMaps.*;
         POLYMERIZATION_TANK_RECIPES.recipeBuilder()
                 .fluidInputs(fluid('oxygen')*1000)
                 .fluidInputs(fluid('ethylene')*144)
-                .fluidInputs(fluid('polyethylene')*144)
+                .fluidInputs(fluid('plastic')*144)
                 .duration(500)
                 .EUt(800)
                 .buildAndRegister();
 
         POLYMERIZATION_TANK_RECIPES.recipeBuilder()
-                .inputs(metaitem('KaminskyCatalyst'))
+                .inputs(metaitem('dustKaminskyCatalyst'))
                 .fluidInputs(fluid('ethylene')*144)
-                .fluidInputs(fluid('polyethylene')*576)
+                .fluidInputs(fluid('plastic')*576)
                 .duration(500)
                 .EUt(800)
                 .buildAndRegister();
 
         POLYMERIZATION_TANK_RECIPES.recipeBuilder()
-                .inputs(metaitem('PhilipsCatalyst'))
+                .inputs(metaitem('dustPhilipsCatalyst'))
                 .fluidInputs(fluid('ethylene')*144)
-                .fluidInputs(fluid('polyethylene')*288)
+                .fluidInputs(fluid('plastic')*288)
                 .duration(500)
                 .EUt(800)
                 .buildAndRegister();
 
         POLYMERIZATION_TANK_RECIPES.recipeBuilder()
-                .inputs(metaitem('ZieglerNattaCatalyst'))
+                .inputs(metaitem('dustZieglerNattaCatalyst'))
                 .fluidInputs(fluid('ethylene')*144)
-                .fluidInputs(fluid('polyethylene')*432)
+                .fluidInputs(fluid('plastic')*432)
                 .duration(500)
                 .EUt(800)
                 .buildAndRegister();
