8c6e17742807ad2e9f3431a27d112f4e2c52b528
BLocky104 <farmermick13@web.de>
2025-03-23 21:05:10 +0100

    Color changes

--- groovy/material/FirstDegreeMaterials.groovy
+++ groovy/material/FirstDegreeMaterials.groovy
@@ -47,66 +47,66 @@ public class FirstDegreeMaterials {
 
         Bromotoluene = new Material.Builder(8003, GTBUtil.gtb("bromotoluene"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0xA68D86)
                 .build();
 
         Bromotoluene.setFormula("C7H7Br", true)
 
         FluorotolueneMixture = new Material.Builder(8004, GTBUtil.gtb("fluorotoluene_mixture"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0x89A3BA)
                 .build();
 
         FluorotolueneMixture.setFormula("(C7H7F)(C3H7NO)", true)
 
         Fluorotoluene = new Material.Builder(8005, GTBUtil.gtb("fluorotoluene"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0xBBC7D2)
                 .build();
 
         Fluorotoluene.setFormula("C7H7F", true)
 
-        Dimethylformamide = new Material.Builder(8006, GTBUtil.gtb("dimethylformamide"))
-                .fluid()
-                .color(0xD8AAAA)
-                .build();
-
-        Dimethylformamide.setFormula("C3H7NO", true)
-
         FluorobenzoicAcid = new Material.Builder(8007, GTBUtil.gtb("fluorobenzoic_acid"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0xD7CFAD)
                 .build();
 
         FluorobenzoicAcid.setFormula("C7H5FO2", true)
 
         FluorobenzoylChloride = new Material.Builder(8008, GTBUtil.gtb("fluorobenzoyl_chloride"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0x6BA3AA)
                 .build();
 
         FluorobenzoylChloride.setFormula("FC6H4COCl", true)
 
         DiphenylSulfone = new Material.Builder(8009, GTBUtil.gtb("diphenyl_sulfone"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0xBDBF9D)
                 .build();
 
         DiphenylSulfone.setFormula("C8H10O2S", true)
 
         AluminiumBromide = new Material.Builder(8010, GTBUtil.gtb("aluminium_bromide"))
                 .dust()
-                .color(0xD8AAAA)
+                .color(0x81A7C6)
                 .build();
 
         AluminiumBromide.setFormula("AlBr3", true)
 
         Fluorobenzene = new Material.Builder(8011, GTBUtil.gtb("fluorobenzene"))
                 .fluid()
-                .color(0xD8AAAA)
+                .color(0x8C8C8C)
                 .build();
 
         Fluorobenzene.setFormula("(C6H5F)", true)
+
+        CarbonDisulfide = new Material.Builder(8012, GTBUtil.gtb("carbon_disulfide"))
+                .fluid()
+                .color(0xC4BDA2)
+                .build();
+
+        CarbonDisulfide.setFormula("CS2", true)
 
 
 
