d8505a79e12ab6f83c393a4b09c17cce3ec3baa1
BLocky104 <farmermick13@web.de>
2025-03-25 20:37:15 +0100

    Bacteria Breeding

---
+++ groovy/postInit/Biology/Bacteria Breeding.groovy
@@ -0,0 +1,45 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+
+
+
+        BIO_REACTOR_RECIPES.recipeBuilder()
+                .notConsumable(fluid('yeem_solution')*1000)
+                .notConsumable(fluid('bacterial_growth_medium')*1000)
+                .fluidOutputs(fluid('depleted_bacterial_growth_medium')*1000)
+                .inputs(metaitem('dustCupriavidusNecator')* 1)
+                .outputs(metaitem('dustCupriavidusNecator')* 2)
+                .duration(800)
+                .EUt(20000)
+                .buildAndRegister();
+
+        BIO_REACTOR_RECIPES.recipeBuilder()
+                .notConsumable(fluid('yeem_solution')*1000)
+                .notConsumable(fluid('bacterial_growth_medium')*1000)
+                .fluidOutputs(fluid('depleted_bacterial_growth_medium')*1000)
+                .inputs(metaitem('dustIriditus')* 1)
+                .outputs(metaitem('dustIriditus')* 2)
+                .duration(800)
+                .EUt(20000)
+                .buildAndRegister();
+
+        BIO_REACTOR_RECIPES.recipeBuilder()
+                .notConsumable(fluid('yeem_solution')*1000)
+                .notConsumable(fluid('bacterial_growth_medium')*1000)
+                .fluidOutputs(fluid('depleted_bacterial_growth_medium')*1000)
+                .inputs(metaitem('dustEschericiaColi')* 1)
+                .outputs(metaitem('dustEschericiaColi')* 2)
+                .duration(800)
+                .EUt(20000)
+                .buildAndRegister();
+
+        BIO_REACTOR_RECIPES.recipeBuilder()
+                .notConsumable(fluid('yeem_solution')*1000)
+                .notConsumable(fluid('bacterial_growth_medium')*1000)
+                .fluidOutputs(fluid('depleted_bacterial_growth_medium')*1000)
+                .inputs(metaitem('dustRhizobium')* 1)
+                .outputs(metaitem('dustRhizobium')* 2)
+                .duration(800)
+                .EUt(20000)
+                .buildAndRegister();
