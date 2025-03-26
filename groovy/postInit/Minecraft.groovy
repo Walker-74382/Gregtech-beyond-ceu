476face79f4be9315afa234126d4e236073be0e2
BLocky104 <farmermick13@web.de>
2025-03-27 00:53:43 +0100

    Minecraft Armor Reworks

---
+++ groovy/postInit/Minecraft.groovy
@@ -0,0 +1,131 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateGold')*4)
+                .circuitMeta(4)
+                .outputs(item('minecraft:golden_boots'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateGold')*7)
+                .circuitMeta(7)
+                .outputs(item('minecraft:golden_leggings'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateGold')*5)
+                .circuitMeta(5)
+                .outputs(item('minecraft:golden_helmet'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateGold')*8)
+                .circuitMeta(8)
+                .outputs(item('minecraft:golden_chestplate'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateDiamond')*4)
+                .circuitMeta(4)
+                .outputs(item('minecraft:diamond_boots'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateDiamond')*7)
+                .circuitMeta(7)
+                .outputs(item('minecraft:diamond_leggings'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateDiamond')*5)
+                .circuitMeta(5)
+                .outputs(item('minecraft:diamond_helmet'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateDiamond')*8)
+                .circuitMeta(8)
+                .outputs(item('minecraft:diamond_chestplate'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateIron')*4)
+                .circuitMeta(4)
+                .outputs(item('minecraft:iron_boots'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateIron')*7)
+                .circuitMeta(7)
+                .outputs(item('minecraft:iron_leggings'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateIron')*5)
+                .circuitMeta(5)
+                .outputs(item('minecraft:iron_helmet'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateIron')*8)
+                .circuitMeta(8)
+                .outputs(item('minecraft:iron_chestplate'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('ringIron')*4)
+                .circuitMeta(4)
+                .outputs(item('minecraft:chainmail_boots'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('ringIron')*7)
+                .circuitMeta(7)
+                .outputs(item('minecraft:chainmail_leggings'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('ringIron')*5)
+                .circuitMeta(5)
+                .outputs(item('minecraft:chainmail_helmet'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('ringIron')*8)
+                .circuitMeta(8)
+                .outputs(item('minecraft:chainmail_chestplate'))
+                .duration(200)
+                .EUt(8)
+                .buildAndRegister();
