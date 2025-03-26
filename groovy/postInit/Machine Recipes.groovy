b9b4e5c25f8897f96caebb7057c1d3e25776c915
BLocky104 <farmermick13@web.de>
2025-03-27 00:53:31 +0100

    port some erroring Machine recipes

---
+++ groovy/postInit/Machine Recipes.groovy
@@ -0,0 +1,45 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .duration(200)
+                .EUt(20)
+                .outputs(item('gtb:gtb_multiblock_casing', 8))
+                .inputs(metaitem('boltSilver'))
+                .inputs(metaitem('frameIron'))
+                .fluidInputs(fluid('alumino_silicate_wool_solution')*288)
+                .buildAndRegister();
+
+        cracfting.addShaped("gregtech:catalytic_reformation_unit_machine", item('gregtech:machine', 3007),[
+                ["ore('plateEglinSteel'), ore('circuitEv') ,ore('plateEglinSteel')"],
+                ["ore('circuitEv'), item('gregtech:machine', 988) , ore('circuitEv')"],
+                ["ore('plateEglinSteel') , ore('circuitEv') ,ore('plateEglinSteel')"]])
+            
+        cracfting.addShaped("gregtech:solid_fuel_generator_machine", item('gregtech:machine', 3008),[
+                ["ore('plateLowQualitySteel'), ore('rotorBronze'), ore('plateLowQualitySteel')"],
+                ["ore('rotorBronze'), item('gregtech:machine_casing'), ore('rotorBronze')"],
+                ["ore('plateLowQualitySteel'), ore('rotorBronze') ,ore('plateLowQualitySteel')"]])
+
+        
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(NEODYMIUM_MAGNET, 2)
+                .inputs(ore('circuitHv'))
+                .inputs(item('gregtech:machine', 985))
+                .inputs(metaitem('plateSteel')* 2)
+                .inputs(metaitem('robot.arm.hv')*2)
+                .outputs(metaitem('gtb:gravity_separator'))
+                .duration(200)
+                .EUt(70)
+                .buildAndRegister();
+
+        ASSEMBLER_RECIPES.recipeBuilder()
+                .inputs(metaitem('plateSiliconCarbide')*4)
+                .inputs(ore('circuitHv')* 2)
+                .inputs(metaitem('frameSiliconCarbide'))
+                .inputs(metaitem('electric.pump.hv')*2)
+                .inputs(metaitem('voltage_coil.mv'))
+                .outputs(metaitem('gtb:high_temp_distillation_tower'))
+                .duration(200)
+                .EUt(120)
+                .buildAndRegister();
