5ed7deb6c427e001a9c08d9eedf3e3cf69b31eb0
BLocky104 <farmermick13@web.de>
2025-03-27 00:50:15 +0100

    Create MaterialPropertyAddition.groovy

---
+++ groovy/material/MaterialPropertyAddition.groovy
@@ -0,0 +1,30 @@
+package material;
+
+import static gregtech.api.unification.material.Materials.*;
+
+import gregtech.api.fluids.FluidBuilder;
+import gregtech.api.fluids.store.FluidStorageKeys;
+import gregtech.api.unification.material.properties.*;
+import static gregtech.api.unification.material.info.MaterialIconSet.*;
+import static gregtech.api.unification.material.info.MaterialFlags.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gtb.api.unification.materials.GTBMaterials.*;
+
+public class MaterialPropertyAddition {
+
+  public static void register() {
+
+
+        
+        Flerovium.setProperty(PropertyKey.DUST, new DustProperty());
+
+
+
+
+
+
+
+
+
+
+  }}
