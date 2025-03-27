19efbe13179608f27bbbdc96071e313850be47f0
BLocky104 <farmermick13@web.de>
2025-03-28 00:05:49 +0100

    remove old Titanium Line recipes

--- groovy/postInit/Chemistry/Inorganic Chemistry/Elements/Titanium.groovy
+++ groovy/postInit/Chemistry/Inorganic Chemistry/Elements/Titanium.groovy
@@ -86,3 +86,8 @@ import static gtb.api.recipes.GTBRecipeMaps.*;
                 .duration(460)
                 .EUt(800)
                 .buildAndRegister();
+
+// Hot Titanium Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(480, [metaitem('dustMagnesium') * 2], [fluid('titanium_tetrachloride') * 1000 * 1000])
+// Carbon Monoxide Gas * 2000
+mods.gregtech.chemical_reactor.removeByInput(480, [metaitem('dustCarbon') * 2, metaitem('dustRutile')], [fluid('chlorine') * 4000 * 4000])
