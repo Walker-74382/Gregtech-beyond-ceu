4f86b031fe09c01d7611546531989d0b517e1aa8
BLocky104 <farmermick13@web.de>
2025-03-27 02:21:29 +0100

    Barium Processing

---
+++ groovy/postInit/Chemistry/Inorganic Chemistry/Elements/Barium.groovy
@@ -0,0 +1,60 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+        BLAST_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustBarite')*6)
+                .inputs(metaitem('dustCarbon')*2)
+                .fluidOutputs(fluid('carbon_dioxide')*2000)
+                .outputs(metaitem('barium_sulfide')*2)
+                .duration(300)
+                .EUt(80)
+                .buildAndRegister();
+        
+        CHEMICAL_RECIPES.recipeBuilder()
+                .inputs(metaitem('barium_sulfide')*2)
+                .fluidInputs(fluid('carbon_dioxide')*1000)
+                .fluidInputs(fluid('water')*1000)
+                .fluidOutputs(fluid('hydrogen_sulfide')*1000)
+                .outputs(metaitem('dustBariumCarbonate')*5)
+                .duration(200)
+                .EUt(50)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustBariumCarbonate')*5)
+                .fluidOutputs(fluid('carbon_dioxide')*1000)
+                .outputs(metaitem('dustBariumOxide')*2)
+                .circuitMeta(1)
+                .duration(200)
+                .EUt(50)
+                .buildAndRegister();
+
+        BLAST_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustBariumOxide')*8)
+                .inputs(metaitem('dustAluminium')*2)
+                .outputs(metaitem('ingotBarium')*3)
+                .outputs(metaitem('dustBariumAluminate')*7)
+                .duration(90)
+                .blastFurnaceTemp(700)
+                .EUt(250)
+                .buildAndRegister();
+
+        CENTRIFUGE_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustBariumAluminate')*7)
+                .outputs(metaitem('dustBariumOxide')*2)
+                .outputs(metaitem('dustAlumina')*5)
+                .duration(90)
+                .EUt(250)
+                .buildAndRegister();
+
+        //Witherite
+
+        CENTRIFUGE_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustWitherite')*5)
+                .outputs(metaitem('dustBariumCarbonate')*5)
+                .chancedoutput(metaitem('dustBarite'), 44, 0)
+                .chancedoutput(metaitem('dustGalena'), 77, 0)
+                .chancedoutput(metaitem('dustCalcite'), 10, 0)
+                .duration(290)
+                .EUt(150)
+                .buildAndRegister();
