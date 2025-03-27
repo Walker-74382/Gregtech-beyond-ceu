78280b7347c0b661576efad0eac28b40d4d18ee7
BLocky104 <farmermick13@web.de>
2025-03-27 02:21:39 +0100

    Calcium Processing

---
+++ groovy/postInit/Chemistry/Inorganic Chemistry/Elements/Calcium.groovy
@@ -0,0 +1,11 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+
+
+
+// Calcium Dust * 5
+mods.gregtech.electrolyzer.removeByInput(60, [metaitem('dustApatite') * 9], null)
+
+// Phosphorus Dust * 1
+mods.gregtech.electrolyzer.removeByInput(30, [metaitem('dustPhosphate') * 5], null)
