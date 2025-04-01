142f423d26793b58d41bdae54be39a6f3f6a3309
BLocky104 <farmermick13@web.de>
2025-04-01 03:45:06 +0200

    revert MaterialPropertyAddition.groovy

--- groovy/material/MaterialPropertyAddition.groovy
+++ groovy/material/MaterialPropertyAddition.groovy
@@ -8,38 +8,39 @@ import gregtech.api.unification.material.properties.*;
 import static gregtech.api.unification.material.info.MaterialIconSet.*;
 import static gregtech.api.unification.material.info.MaterialFlags.*;
 import static gregtech.api.unification.material.Materials.*;
-import gregtech.api.GregTechAPI.PostMaterialEvent;
 import static gtb.api.unification.materials.GTBMaterials.*;
 
 public class MaterialPropertyAddition {
 
   public static void register() {
-
-
-      event_manager.listen { PostMaterialEvent event ->
-            material('nether_star').setProperty(PropertyKey.FLUID, new FluidProperty())
-            material('blue_alloy').addFlags("generate_fine_wire")
-            material('certus_quartz').setProperty(PropertyKey.FLUID, new FluidProperty())
-            material('certus_quartz').setMaterialRGB(0xa7cafc) //0xa7cafc
-            material('nether_quartz').setProperty(PropertyKey.FLUID, new FluidProperty())
-            material('nether_quartz').addFlags("generate_lens")
-            material('quartzite').setProperty(PropertyKey.FLUID, new FluidProperty())
-            material('blue_topaz').addFlags("generate_lens")
-            material('ender_pearl').addFlags("generate_lens")
-            material('topaz').addFlags("generate_lens")
-            material('lapis').addFlags("generate_lens")
-            material('amethyst').addFlags("generate_lens")
-            material('garnet_yellow').addFlags("generate_lens")
       
-            material('brass').addFlags("generate_round")
-            material('electrum').addFlags("generate_round")
-            material('chrome').addFlags("generate_round")
-            material('platinum').addFlags("generate_round")
-            material('iridium').addFlags("generate_round")
+            CertusQuartz.setMaterialRGB(0xA7CAFC); //0xa7cafc
 
-            material('flerovium').addFlags("generate_dust")
-            material('barium').addFlags("generate_ingot")
+            Flerovium.setProperty(PropertyKey.DUST, new DustProperty());
+            Barium.setProperty(PropertyKey.INGOT, new IngotProperty());
+            NetherQuartz.setProperty(PropertyKey.FLUID, new FluidProperty(FluidStorageKeys.LIQUID, new FluidBuilder()));
+            CertusQuartz.setProperty(PropertyKey.FLUID, new FluidProperty(FluidStorageKeys.LIQUID, new FluidBuilder()));
+            Quartzite.setProperty(PropertyKey.FLUID, new FluidProperty(FluidStorageKeys.LIQUID, new FluidBuilder()));
+
+            Iridium.addFlags(GENERATE_ROUND);
+            Platinum.addFlags(GENERATE_ROUND);
+            Chrome.addFlags(GENERATE_ROUND);
+            Electrum.addFlags(GENERATE_ROUND);
+            Brass.addFlags(GENERATE_ROUND);
+            Iridium.addFlags(GENERATE_ROUND);
+
+            GarnetYellow.addFlags(GENERATE_LENS);
+            Amethyst.addFlags(GENERATE_LENS);
+            Lapis.addFlags(GENERATE_LENS);
+            Topaz.addFlags(GENERATE_LENS);
+            EnderPearl.addFlags(GENERATE_LENS);
+            BlueTopaz.addFlags(GENERATE_LENS);
+            NetherQuartz.addFlags(GENERATE_LENS);
+
+            BlueAlloy.addFlags(GENERATE_FINE_WIRE);
+
+
+            Phosphate.setProperty(PropertyKey.ORE, new OreProperty());
       
       }
   }
-}
