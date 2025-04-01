f3e7a88e344ccab62977375c9d72a389412a207f
BLocky104 <farmermick13@web.de>
2025-04-01 03:41:40 +0200

    update run Config for Globals.groovy

---
+++ groovy/globals/Globals.groovy
@@ -0,0 +1,21 @@
+import gregtech.api.unification.material.Material;
+
+
+import static gregtech.api.fluids.FluidConstants.*;
+
+class Globals {
+
+    public static void main (String[] args) {
+
+    }
+
+    public static mod_priority = [
+        "minecraft",
+        "gregtech",
+        "gcym",
+        "galacticraft"
+    ]
+
+
+
+}
