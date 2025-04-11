d5620d7b2b56fabb22c6a2c7c1067aaa4bf4e393
BLocky104 <farmermick13@web.de>
2025-04-11 20:17:37 +0200

    begin work on AE2

---
+++ groovy/postInit/Mods/Applied Energistics.groovy
@@ -0,0 +1,13 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+
+crafting.removeByOutput(item('appliedenergistics2:inscriber'))
+crafting.removeByOutput(item('ae2stuff:inscriber'))
+
+
+
+
+
+oreDict.remove("crystalCertusQuartz", item('appliedenergistics2:material'))
+oreDict.remove("crystalFluix", item('appliedenergistics2:material', 7))
