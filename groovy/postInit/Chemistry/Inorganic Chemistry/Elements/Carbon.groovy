853069f696072bad652b582f2e2c424877beede0
BLocky104 <farmermick13@web.de>
2025-04-21 03:39:08 +0200

    remove stupid CO recipe

---
+++ groovy/postInit/Chemistry/Inorganic Chemistry/Elements/Carbon.groovy
@@ -0,0 +1,7 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+// Carbon Monoxide Gas * 2000
+mods.gregtech.chemical_reactor.removeByInput(7, [metaitem('dustCarbon')], [fluid('carbon_dioxide') * 1000 * 1000])
+
+
