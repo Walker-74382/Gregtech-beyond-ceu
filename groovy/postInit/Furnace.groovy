c93e9af45f0ae2b60c83757d4e5271742e1c23e9
BLocky104 <farmermick13@web.de>
2025-04-21 03:54:35 +0200

    Create Furnace.groovy

---
+++ groovy/postInit/Furnace.groovy
@@ -0,0 +1,146 @@
+        // Bornite
+        furnace.removeByInput(metaitem('dustImpureBornite'));
+        furnace.removeByInput(metaitem('dustBornite'));
+        furnace.removeByInput(metaitem('dustPureBornite'));
+        furnace.removeByInput(metaitem('crushedBornite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedBornite'));
+        furnace.removeByInput(metaitem('crushedPurifiedBornite'));
+        furnace.removeByInput(item('gregtech:ore_bornite_0'));
+        
+        // Chalcocite
+        furnace.removeByInput(metaitem('dustImpureChalcocite'));
+        furnace.removeByInput(metaitem('dustChalcocite'));
+        furnace.removeByInput(metaitem('dustPureChalcocite'));
+        furnace.removeByInput(metaitem('crushedChalcocite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedChalcocite'));
+        furnace.removeByInput(metaitem('crushedPurifiedChalcocite'));
+        furnace.removeByInput(item('gregtech:ore_chalcocite_0'));
+
+        // Tetrahedrite
+        furnace.removeByInput(metaitem('dustImpureTetrahedrite'));
+        furnace.removeByInput(metaitem('dustTetrahedrite'));
+        furnace.removeByInput(metaitem('dustPureTetrahedrite'));
+        furnace.removeByInput(metaitem('crushedTetrahedrite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedTetrahedrite'));
+        furnace.removeByInput(metaitem('crushedPurifiedTetrahedrite'));
+        furnace.removeByInput(item('gregtech:ore_tetrahedrite_0'));
+
+        // Malachite
+        furnace.removeByInput(metaitem('dustImpureMalachite'));
+        furnace.removeByInput(metaitem('dustMalachite'));
+        furnace.removeByInput(metaitem('dustPureMalachite'));
+        furnace.removeByInput(metaitem('crushedMalachite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedMalachite'));
+        furnace.removeByInput(metaitem('crushedPurifiedMalachite'));
+        furnace.removeByInput(item('gregtech:ore_malachite_0'));
+
+        furnace.removeByInput(metaitem('dustImpureYellowLimonite'));
+        furnace.removeByInput(metaitem('dustYellowLimonite'));
+        furnace.removeByInput(metaitem('dustPureYellowLimonite'));
+        furnace.removeByInput(metaitem('crushedYellowLimonite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedYellowLimonite'));
+        furnace.removeByInput(metaitem('crushedPurifiedYellowLimonite'));
+        furnace.removeByInput(item('gregtech:ore_yellow_limonite_0'));
+        
+        furnace.removeByInput(metaitem('dustImpureMagnetite'));
+        furnace.removeByInput(metaitem('dustMagnetite'));
+        furnace.removeByInput(metaitem('dustPureMagnetite'));
+        furnace.removeByInput(metaitem('crushedMagnetite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedMagnetite'));
+        furnace.removeByInput(metaitem('crushedPurifiedMagnetite'));
+        furnace.removeByInput(item('gregtech:ore_magnetite_0'));
+
+        furnace.removeByInput(metaitem('dustImpureBandedIron'));
+        furnace.removeByInput(metaitem('dustBandedIron'));
+        furnace.removeByInput(metaitem('dustPureBandedIron'));
+        furnace.removeByInput(metaitem('crushedBandedIron'));
+        furnace.removeByInput(metaitem('crushedCentrifugedBandedIron'));
+        furnace.removeByInput(metaitem('crushedPurifiedBandedIron'));
+        furnace.removeByInput(item('gregtech:ore_banded_iron_0'));
+
+        furnace.removeByInput(metaitem('dustImpureBrownLimonite'));
+        furnace.removeByInput(metaitem('dustBrownLimonite'));
+        furnace.removeByInput(metaitem('dustPureBrownLimonite'));
+        furnace.removeByInput(metaitem('crushedBrownLimonite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedBrownLimonite'));
+        furnace.removeByInput(metaitem('crushedPurifiedBrownLimonite'));
+        furnace.removeByInput(item('gregtech:ore_brown_limonite_0'));
+
+        furnace.removeByInput(metaitem('dustImpureBasalticMineralSand'));
+        furnace.removeByInput(metaitem('dustBasalticMineralSand'));
+        furnace.removeByInput(metaitem('dustPureBasalticMineralSand'));
+        furnace.removeByInput(metaitem('crushedBasalticMineralSand'));
+        furnace.removeByInput(metaitem('crushedCentrifugedBasalticMineralSand'));
+        furnace.removeByInput(metaitem('crushedPurifiedBasalticMineralSand'));
+        furnace.removeByInput(item('gregtech:ore_basaltic_mineral_sand_0'));
+
+        furnace.removeByInput(metaitem('dustImpureMolybdenite'));
+        furnace.removeByInput(metaitem('dustMolybdenite'));
+        furnace.removeByInput(metaitem('dustPureMolybdenite'));
+        furnace.removeByInput(metaitem('crushedMolybdenite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedMolybdenite'));
+        furnace.removeByInput(metaitem('crushedPurifiedMolybdenite'));
+        furnace.removeByInput(item('gregtech:ore_molybdenite_0'));
+
+        furnace.removeByInput(metaitem('dustImpurePentlandite'));
+        furnace.removeByInput(metaitem('dustPentlandite'));
+        furnace.removeByInput(metaitem('dustPurePentlandite'));
+        furnace.removeByInput(metaitem('crushedPentlandite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedPentlandite'));
+        furnace.removeByInput(metaitem('crushedPurifiedPentlandite'));
+        furnace.removeByInput(item('gregtech:ore_pentlandite_0'));
+
+        furnace.removeByInput(metaitem('dustImpureGalena'));
+        furnace.removeByInput(metaitem('dustGalena'));
+        furnace.removeByInput(metaitem('dustPureGalena'));
+        furnace.removeByInput(metaitem('crushedGalena'));
+        furnace.removeByInput(metaitem('crushedCentrifugedGalena'));
+        furnace.removeByInput(metaitem('crushedPurifiedGalena'));
+        furnace.removeByInput(item('gregtech:ore_galena_0'));
+
+        furnace.removeByInput(metaitem('dustImpureGarnierite'));
+        furnace.removeByInput(metaitem('dustGarnierite'));
+        furnace.removeByInput(metaitem('dustPureGarnierite'));
+        furnace.removeByInput(metaitem('crushedGarnierite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedGarnierite'));
+        furnace.removeByInput(metaitem('crushedPurifiedGarnierite'));
+        furnace.removeByInput(item('gregtech:ore_garnierite_0'));
+
+        furnace.removeByInput(metaitem('dustImpureCassiterite'));
+        furnace.removeByInput(metaitem('dustCassiterite'));
+        furnace.removeByInput(metaitem('dustPureCassiterite'));
+        furnace.removeByInput(metaitem('crushedCassiterite'));
+        furnace.removeByInput(metaitem('crushedCentrifugedCassiterite'));
+        furnace.removeByInput(metaitem('crushedPurifiedCassiterite'));
+        furnace.removeByInput(item('gregtech:ore_cassiterite_0'));
+
+        furnace.removeByInput(metaitem('dustImpureCassiteriteSand'));
+        furnace.removeByInput(metaitem('dustCassiteriteSand'));
+        furnace.removeByInput(metaitem('dustPureCassiteriteSand'));
+        furnace.removeByInput(metaitem('crushedCassiteriteSand'));
+        furnace.removeByInput(metaitem('crushedCentrifugedCassiteriteSand'));
+        furnace.removeByInput(metaitem('crushedPurifiedCassiteriteSand'));
+        furnace.removeByInput(item('gregtech:ore_cassiterite_sand_0'));
+
+        
+
+        furnace.removeByInput(metaitem('dustSolderingAlloy'));
+        furnace.removeByInput(metaitem('dustFullerenePolymerTetrix'));
+        furnace.removeByInput(metaitem('dustKaptonE'));
+        furnace.removeByInput(metaitem('dustCupronickel'));
+        furnace.removeByInput(metaitem('dustFullerenePolymerMatrix'));
+        furnace.removeByInput(metaitem('dustZylon'));
+        furnace.removeByInput(metaitem('dustNomex'));
+        furnace.removeByInput(metaitem('dustKevlar'));
+        furnace.removeByInput(metaitem('dustBorosilicateGlass'));
+        furnace.removeByInput(metaitem('dustPurePlatinum'));
+        furnace.removeByInput(metaitem('crushedPlatinum'));
+        furnace.removeByInput(metaitem('crushedCentrifugedPlatinum'));
+        furnace.removeByInput(metaitem('crushedPurifiedPlatinum'));
+        furnace.removeByInput(item('gregtech:ore_platinum_0'));
+        furnace.removeByInput(metaitem('dustKaptonK'));
+        furnace.removeByInput(metaitem('dustImpurePlatinum'));
+        furnace.removeByInput(metaitem('dustPlatinum'));
+        furnace.removeByInput(metaitem('dustInvar'));
+        furnace.removeByOutput(item('appliedenergistics2:material', 5));
+        
