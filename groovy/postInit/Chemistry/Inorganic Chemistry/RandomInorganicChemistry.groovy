78ccc4ddc410833bcf8e073b10a2b44fd95fae72
BLocky104 <farmermick13@web.de>
2025-04-21 03:54:09 +0200

    Update RandomInorganicChemistry.groovy

--- groovy/postInit/Chemistry/Inorganic Chemistry/RandomInorganicChemistry.groovy
+++ groovy/postInit/Chemistry/Inorganic Chemistry/RandomInorganicChemistry.groovy
@@ -106,6 +106,124 @@ mods.gregtech.electrolyzer.removeByInput(30, [metaitem('dustCalciumChloride') * 
                 .EUt(110)
                 .duration(300)
                 .buildAndRegister();
+        
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('chlorine')*1000)
+                .inputs(metaitem('dustIron'))
+                .fluidOutputs(fluid('iron_chloride')*1000)
+                .EUt(110)
+                .duration(300)
+                .buildAndRegister();
 
+        //Trimethyl Tin Chloride
 
-    
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('hydrobromic_acid')*1000)
+                .fluidInputs(fluid('methanol')*1000)
+                .fluidOutputs(fluid('methyl_bromide')*1000)
+                .duration(300)
+                .EUt(40)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('methyl_bromide')*1000)
+                .notConsumable(metaitem('dustIodine')*2)
+                .notConsumable(fluid('dry_diethyl_ether')*1000)
+                .inputs(metaitem('dustMagnesium'))
+                .notConsumable(fluid('nitrogen')*2000)
+                .notConsumable(metaitem('fume_hood'))
+                .fluidOutputs(fluid('methylmagnesium_bromide')*1000)
+                .duration(300)
+                .EUt(40)
+                .buildAndRegister();
+
+        FRACTIONAL_DISTILLATION_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('diethyl_ether')*1000)
+                .notConsumable(fluid('difluorobenzophenone')*1000)
+                .fluidOutputs(fluid('dry_diethyl_ether')*1000)
+                .duration(300)
+                .EUt(240)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('chlorine')*4000)
+                .inputs(metaitem('dustTin'))
+                .outputs(metaitem('dustTinChloride'))
+                .duration(300)
+                .EUt(380)
+                .buildAndRegister();
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustTinChloride'))
+                .fluidInputs(fluid('methylmagnesium_bromide')*1000)
+                .fluidOutputs(fluid('trimethyl_tin_chloride') * 1000)
+                .outputs(metaitem('dustMagnesiumChloride')*3)
+                .duration(150)
+                .EUt(480)
+                .buildAndRegister();
+
+        //Chichibabin Synthesis
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('formaldehyde')*1000)
+                .fluidInputs(fluid('ammonia')*1000)
+                .fluidInputs(fluid('gtfo_acetaldehyde')*1000)
+                .fluidOutputs(fluid('pyridine')*1000)
+                .fluidOutputs(fluid('water')*1000)
+                .duration(150)
+                .EUt(480)
+                .buildAndRegister();
+
+        //Dichloromethane
+
+        UV_LIGHT_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('methane')*1000)
+                .fluidInputs(fluid('chlorine')*2000)
+                .fluidOutputs(fluid('hydrogen_chloride')*1000)
+                .fluidOutputs(fluid('dichloromethane')*1000)
+                .duration(100)
+                .EUt(250)
+                .buildAndRegister();
+
+        //Diluted Acetone
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('distilled_water')*1000)
+                .fluidInputs(fluid('acetone')*1000)
+                .fluidOutputs(fluid('diluted_acetone')*1000)
+                .duration(100)
+                .EUt(40)
+                .buildAndRegister();
+
+        //Phosphorus Oxychloride
+
+        CHEMICAL_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('oxygen')*2000)
+                .fluidInputs(fluid('phosphoric_acid')*1000)
+                .fluidInputs(fluid('phosphorus_oxychloride')*1000)
+                .duration(200)
+                .EUt(40)
+                .buildAndRegister();
+
+        //Barium Peroxide
+
+        BLAST_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('oxygen')*2000)
+                .inputs(metaitem('dustBariumOxide')*4)
+                .outputs(metaitem('dustBariumPeroxide')*6)
+                .duration(200)
+                .EUt(40)
+                .blastFurnaceTemp(2300)
+                .buildAndRegister();
+
+        //Boron Oxide
+
+        ELECTROLYZER_RECIPES.recipeBuilder()
+                .inputs(metaitem('dustBoronOxide')*2)
+                .notConsumable(metaitem('stickIron'))
+                .notConsumable(metaitem('stickPlatinum'))
+                .fluidOutputs(fluid('oxygen')*1000)
+                .outputs(metaitem('dustBoron'))
+                .duration(200)
+                .EUt(40)
+                .buildAndRegister();
