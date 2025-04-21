7acbd3087b767f8d5e4b59852e121184abc90b62
BLocky104 <farmermick13@web.de>
2025-04-21 03:53:58 +0200

    Butanol and Methanol Fractional Distillation

--- groovy/postInit/Chemistry/Inorganic Chemistry/Purification.groovy
+++ groovy/postInit/Chemistry/Inorganic Chemistry/Purification.groovy
@@ -10,7 +10,7 @@ mods.gregtech.chemical_reactor.removeByInput(96, [metaitem('circuit.integrated')
 // Methanol * 1000
 mods.gregtech.chemical_reactor.removeByInput(96, [metaitem('circuit.integrated').withNbt(['Configuration': 3]), metaitem('dustCarbon')], [fluid('hydrogen') * 4000 * 4000, fluid('oxygen') * 1000 * 1000])
 
-        DISTILLATION_RECIPES.recipeBuilder() //FRACTIONAL DISTILLATION
+        FRACTIONAL_DISTILLATION_RECIPES.recipeBuilder()
             .fluidInputs(fluid('methanol')*1000)
             .fluidOutputs(fluid('methanol_100')*800)
             .fluidOutputs(fluid('water')*200)
@@ -18,7 +18,7 @@ mods.gregtech.chemical_reactor.removeByInput(96, [metaitem('circuit.integrated')
             .EUt(120)
             .buildAndRegister();
 
-        DISTILLATION_RECIPES.recipeBuilder() //FRACTIONAL DISTILLATION
+        FRACTIONAL_DISTILLATION_RECIPES.recipeBuilder()
             .fluidInputs(fluid('nbutanol')*1000)
             .fluidOutputs(fluid('butanol_100')*800)
             .fluidOutputs(fluid('water')*200)
