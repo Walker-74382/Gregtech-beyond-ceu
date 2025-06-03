bef2a16127b8d33335cf1068b7d789a647bd7169
BLocky104 <farmermick13@web.de>
2025-06-03 19:46:05 +0200

    gate Retro Backpacks

---
+++ groovy/postInit/Mods/RetroBackpacks.groovy
@@ -0,0 +1,9 @@
+
+
+crafting.remove("retro_sophisticated_backpacks:upgrade_base")
+
+crafting.addShaped('backpacks_upgrade_base', item('retro_sophisticated_backpacks:upgrade_base'), [
+[ore('screwSteel'), ore('string'), ore('screwSteel')], 
+[ore('string'), item('gregtech:meta_item_2', 259), ore('string')], 
+[ore('screwSteel'), ore('string'), ore('screwSteel')]
+])
