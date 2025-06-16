940b8aec2a6500bc701e5097d228072649c6e5b9
BLocky104 <farmermick13@web.de>
2025-06-16 03:08:24 +0200

    recipe for Morphy Tool

---
+++ groovy/postInit/Mods/Morph O Tool.groovy
@@ -0,0 +1,7 @@
+crafting.addShaped("morphy_tool", item('morphtool:tool'), [
+[null, ore('dyeLime'), ore('dyeBlue')], 
+[null, ore('stickIron'), ore('dyeRed')], 
+[ore('stickIron'), null, null]
+])
+
+crafting.remove('morphtool:tool');
