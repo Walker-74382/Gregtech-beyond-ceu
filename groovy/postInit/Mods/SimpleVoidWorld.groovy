6d2f3522e4ada3a4968dd461f32fb1703e46fd87
BLocky104 <farmermick13@web.de>
2025-06-16 03:07:47 +0200

    recipe for Simple Void World

---
+++ groovy/postInit/Mods/SimpleVoidWorld.groovy
@@ -0,0 +1,7 @@
+crafting.remove("simplevoidworld:portal")
+
+crafting.addShaped("void_portal", item('simplevoidworld:portal'), [
+[ore('gemEnderEye'), item('gregtech:meta_item_1', 128), ore('gemEnderEye')], 
+[item('gregtech:meta_item_1', 128), ore('blockDiamond'), item('gregtech:meta_item_1', 128)], 
+[ore('gemEnderEye'), item('gregtech:meta_item_1', 128), ore('gemEnderEye')]
+])
