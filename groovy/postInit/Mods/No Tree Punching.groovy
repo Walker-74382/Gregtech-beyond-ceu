c22cfc1510aef1873e2ab1a64bc89e0b83da6adf
BLocky104 <farmermick13@web.de>
2025-06-16 03:08:15 +0200

    NoTreePunching Integration

---
+++ groovy/postInit/Mods/No Tree Punching.groovy
@@ -0,0 +1,17 @@
+
+crafting.remove('notreepunching:misc/clay_tool_clay_brick_2')
+crafting.remove('notreepunching:misc/clay_tool_clay_brick')
+
+
+furnace.removeByInput(item('notreepunching:clay_brick'));
+
+crafting.remove('notreepunching:misc:tools/flint_knife')
+crafting.remove('notreepunching:misc:tools/iron_knife')
+crafting.remove('notreepunching:misc:tools/gold_knife')
+crafting.remove('notreepunching:misc:tools/diamond_knife')
+crafting.remove('notreepunching:misc:tools/tin_knife')
+crafting.remove('notreepunching:misc:tools/copper_knife')
+crafting.remove('notreepunching:misc:tools/steel_knife')
+crafting.remove('notreepunching:misc:tools/bronze_knife')
+
+furnace.removeByInput(item('notreepunching:pottery/bucket'));
