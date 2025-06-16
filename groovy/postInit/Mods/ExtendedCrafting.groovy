3d69bc77079e1f300f4d3e90f65abf7f60ee570e
BLocky104 <farmermick13@web.de>
2025-06-16 03:09:06 +0200

    EC Integration

---
+++ groovy/postInit/Mods/ExtendedCrafting.groovy
@@ -0,0 +1,9 @@
+crafting.remove('extendedcrafting:handheld_table')
+
+crafting.addShaped("handheld_table", item('extendedcrafting:handheld_table'), [
+[null, ore('screwSteel'), ore('workbench')], 
+[null, ore('stickWood'), ore('screwSteel')], 
+[ore('stickWood'), null, null]
+])
+
+crafting.remove('extendedcrafting:black_iron_ingot')
